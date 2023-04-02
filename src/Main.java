38efdb9e8bd2ccc73202d3e8e8c3b0f5c58f7704
Abhinav Kumar <kumaraka2000@gmail.com>
2023-04-02 18:09:42 +0530

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+public class Main
+{
+	public static void main(String[] args)
+	{
+		List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 6);
+		//Using forEach()
+		integerList.stream().map(n-> n + 10).forEach(System.out::println);
+		//Using Collectors
+		List<Integer> addedIntegerList = integerList.stream().map(n -> n + 25).collect(Collectors.toList());
+		System.out.println(addedIntegerList);
+	}
 }
